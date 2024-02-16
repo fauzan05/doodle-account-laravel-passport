eafb8b3112ce4c5b73465e5da36645ee49f0254d
Fauzan Nur Hidayat <fauzannurhidayat8@gmail.com>
2024-02-17 02:25:05 +0700

    add: adding timeline component

---
+++ resources/js/components/todos/Timeline.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="card">
+        <div class="card-header">Timeline</div>
+        <div class="card-body">
+            <div class="media">
+                <img class="mr-3" src="https://placehold.it/64x64" alt="">
+                <div class="media-body">
+                    <h5 class="mt-0">Media Heading</h5>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
